fd267699262dc78140b976d590ea116a146f999c
cyanial <chenjie3323@sjtu.edu.cn>
2022-04-22 18:06:04 +0800

    implement

--- unionfind.go
+++ unionfind.go
@@ -1,1 +1,58 @@
 package unionfind
+
+type UnionFind struct {
+	count  int
+	parent []int
+	size   []int
+}
+
+func New(n int) *UnionFind {
+	parent := make([]int, n)
+	size := make([]int, n)
+	for i := 0; i < n; i++ {
+		parent[i] = i
+		size[i] = 1
+	}
+	return &UnionFind{
+		count:  n,
+		parent: parent,
+		size:   size,
+	}
+}
+
+func (u *UnionFind) Find(x int) int {
+	// for u.parent[x] != x {
+	// 	x = u.parent[x]
+	// }
+	// return x
+
+	// 路径压缩
+	if u.parent[x] != x {
+		u.parent[x] = u.Find(u.parent[x])
+	}
+	return u.parent[x]
+}
+
+func (u *UnionFind) Union(p, q int) {
+	rootP, rootQ := u.Find(p), u.Find(q)
+	if rootP == rootQ {
+		return
+	}
+	// u.parent[rootP] = rootQ
+	if u.size[rootP] > u.size[rootQ] {
+		u.parent[rootQ] = rootP
+		u.size[rootP] += u.size[rootQ]
+	} else {
+		u.parent[rootP] = rootQ
+		u.size[rootQ] += u.size[rootP]
+	}
+	u.count--
+}
+
+func (u *UnionFind) Connected(p, q int) bool {
+	return u.Find(p) == u.Find(q)
+}
+
+func (u *UnionFind) Count() int {
+	return u.count
+}
--- unionfind_test.go
+++ unionfind_test.go
@@ -1,1 +1,12 @@
 package unionfind
+
+import "testing"
+
+func TestUnionFind(t *testing.T) {
+	u := New(10)
+	u.Union(1, 2)
+	u.Union(3, 4)
+	if u.Count() != 8 {
+		t.Errorf("Union failed")
+	}
+}
